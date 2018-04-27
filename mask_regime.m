fa1b94413606791837b02421d53b7fe06116c33a
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2018-04-27 15:44:08 -0600

    Latest updates.

--- mask_regime.m
+++ mask_regime.m
@@ -7,7 +7,10 @@
 %
 % Purpose: Using the StartEndLUT mask out the data outside the requested regime
 %
-% Notes: 
+% Notes: variables in the StartEndLUT are: mbegind, mendind, bbegind,
+% bendind, signifying the beginning and the end of the monsoon and break
+% periods. The length of these variables signifies the respective number of
+% monsoon and break periods.
 %________________________________________________________________________________________________
 
 %######################## User Config ##################################%
@@ -18,41 +21,47 @@ matpath = '/d1/dadriaan/paper/data/matfiles';
 %#######################################################################%
 
 % Load the profiler data
-load([matpath,'/profiler.mat'],'Datenum','VertVel','regime','Dbz','Dopvel','agl');
+load([matpath,'/profiler.mat'],'Datenum','VertVel','Dbz','Dopvel','Uwind','Vwind','mask');
 
 % Load the information in the lookup table
 load([matpath,'/StartEndLUT.mat']);
 
-% Create a new 2D time/height matrix to hold the mask
-mask = zeros(size(VertVel));
+% Create a new vector to hold the mask for regime. Here the wind component
+% should be 1D only, at the height that was requested in profiler_driver.m
+rmask = zeros(size(VertVel));
 
 % Mask out the data that we don't need
 if strcmp(regime,'break')
-    begins = bbsdn;
-    ends = besdn;
+    begins = bbegind;
+    ends = bendind;
 else
-    begins = mbsdn;
-    ends = mesdn;
+    begins = mbegind;
+    ends = mendind;
 end
 
-% Loop over the number of begins (one for each regime chunk we have) and turn the data to good
-% Good = 1
+% Loop over the number of begins (one for each regime chunk we have) and
+% note where we want to save data.
+% save = 1
+% mask = 0
 nstarts = length(begins);
 for s=1:nstarts
-   mask(:,find(Datenum==begins(s)):find(Datenum==ends(s))) = 1;
+   %mask(:,find(Datenum==begins(s)):find(Datenum==ends(s))) = 1;
+   rmask(begins(s):ends(s)) = 1;
 end
 
 % Now turn data to the enumeration that means "outside regime"
 % 2 = outside regime
-mask(mask==0) = 2;
+mask(rmask==0) = 2;
 
 % Apply the mask to all the data
-VertVel(mask==2) = nan;
-Dbz(mask==2) = nan;
-Dopvel(mask==2) = nan;
+VertVel(mask>1) = nan;
+Uwind(mask>1) = nan;
+Vwind(mask>1) = nan;
+Dbz(mask>1) = nan;
+Dopvel(mask>1) = nan;
 
 % Save out the data to a MAT file (APPEND)
-save([matpath,'/profiler.mat'],'VertVel','Dopvel','Dbz','mask','-append');
+save([matpath,'/profiler.mat'],'VertVel','Uwind','Vwind','Dopvel','Dbz','mask','-append');
 
 % Clear out variables we don't need
-clear('VertVel','Datenum','regime','mask','Dopvel','Dbz');
+%clear('VertVel','Datenum','mask','Dopvel','Dbz','mbegind','mendind','bbegind','bendind');
