cacd9e9b789fc0a22f68fdaad393fdf3f0f4f4d9
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2018-04-27 15:42:58 -0600

    Latest updates.

--- find_chunks.m
+++ find_chunks.m
@@ -22,18 +22,22 @@ mingood = 240;
 lev = 3000;
 
 % Debug
-debug = 0;
+debug = 1;
 
 %#######################################################################%
 
 % Load data from MAT file
-load([matpath,'/profiler.mat'],'Datenum','mask','agl','regime','VertVel');
+%load([matpath,'/profiler.mat'],'Datenum','mask','agl','VertVel');
+load([matpath,'/profiler.mat'],'Datenum','mask','VertVel','regime');
+%load([matpath,'/profiler.mat'],'Datenum','mask','agl','regime','VertVel');
+%load([matpath,'/',currmfile],'Datenum','mask','agl','regime','VertVel');
 
 % Create a mask of zeros and ones, where 1 is good, and zero is bad
 mask_boolean = ones(1,length(Datenum));
 
 % Turn off the points where there's data we don't want
-mask_boolean(mask(agl==lev,:)>1) = 0;
+%mask_boolean(mask(agl==lev,:)>1) = 0;
+mask_boolean(mask>1) = 0;
 
 % Figure out the difference of the mask
 mask_diff = diff(mask_boolean);
@@ -58,6 +62,9 @@ chunklength = chunkend - chunkbegin;
 
 % Print the number of chunks that exceed the minimum
 fprintf(['\n Number of good chunks for ',regime,' is: ',num2str(length(find(chunklength>mingood))),'\n']);
+if length(find(chunklength>mingood))==0
+  error('NO CHUNKS TO PROCESS');
+end
 
 % Debugging
 if debug
@@ -76,12 +83,15 @@ if debug
         tend = chunkend(goodchunks(n));
                 
         fw = [0,0,900,700];
-        figure('visible','off','position',fw);
+        %op = [0,0,1,1];
+        %pp = [0.2500 2.5000 8 6];
+        figure('visible','off','Position',fw);
         
         % Time series of the input velocity
-        figure('visible','off','position',fw);
+        %figure('visible','off','Position',fw,'OuterPosition',op,'PaperPosition',pp);
         subplot(2,1,1)
-        plot(Datenum(tbeg:tend),mask(agl==lev,tbeg:tend));
+        %plot(Datenum(tbeg:tend),mask(agl==lev,tbeg:tend));
+        plot(Datenum(tbeg:tend),mask(tbeg:tend));
         xlabel('Time (UTC)');
         ylabel('Mask');
         title({[regime,' chunk ',num2str(n)],['Begin = ',datestr(Datenum(tbeg))],['End = ',datestr(Datenum(tend))],['Length =',num2str(length(tbeg:tend)),' MIN']})
@@ -89,16 +99,19 @@ if debug
         set(gca,'YLim',[-2.0 2.0]);
         %axis tight;
         subplot(2,1,2)
-        plot(Datenum(tbeg:tend),VertVel(agl==lev,tbeg:tend))
+        %plot(Datenum(tbeg:tend),VertVel(agl==lev,tbeg:tend))
+        plot(Datenum(tbeg:tend),VertVel(tbeg:tend));
         xlabel('Time (UTC)')
         ylabel('Velocity m/s')
         datetick('x',15)
-        saveas(gcf,['mask_',regime,'_chunk_',num2str(n),'.png']);
+        saveas(gcf,['mask_',regime,'_chunk_',sprintf('%02d',n),'.png']);
+        
     end
 end
 
 % Save out the data to a MAT file (APPEND)
 save([matpath,'/profiler.mat'],'Datenum','mask','agl','chunkbegin','chunkend','chunklength','regime','VertVel','-append');
+%save([matpath,currmfile],'Datenum','mask','agl','chunkbegin','chunkend','chunklength','regime','VertVel','-append');
 
 % Clear out variables we don't need
-clear('Datenum','mask','agl','mask_boolean','mask_diff','chunkbegin','chunkend','chunklength','regime','VertVel');
+clear('Datenum','mask','mask_boolean','mask_diff','chunkbegin','chunkend','chunklength','VertVel');
